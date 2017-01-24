d261fb9141889816759def499de990ed8dfc9f34
David Quisenberry and Koji Nakagawa <david@>
2017-01-24 14:07:31 -0800

    add jquery to build first bankaccount object

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,49 @@
+//Business Logic
+
+function BankAccount (fullName, email, password){
+  this.fullName = fullName;
+  this.email = email;
+  this.password = password;
+  this.balance = 0;
+}
+
+BankAccount.prototype.deposit = function(amount){
+    balance += amount;
+};
+
+BankAccount.prototype.withdrawal = function(amount){
+    balance -= amount;
+};
+
 
 
 $(function(){
-  $("form#userInput").submit(function(event){
-    
+  $("form#signup-form").submit(function(event){
+    var fullName = $("#full-name").val();
+    var email = $("#signup-email").val();
+    var password = $("#signup-password").val();
+
+    var newBankAccount = new BankAccount(fullName, email, password);
+    console.log(newBankAccount);
+
+
+    event.preventDefault();
+  });
+});
+
+
+$(function(){
+  $("form#signin-form").submit(function(event){
+
+
+    event.preventDefault();
+  });
+});
+
+
+$(function(){
+  $("form#action-form").submit(function(event){
+
 
     event.preventDefault();
   });
