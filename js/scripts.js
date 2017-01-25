7b1917cae1bdf30bcf937615d110b1e4ea6c09d7
David Quisenberry and Koji Nakagawa <koji@>
2017-01-24 16:52:17 -0800

    change the logic of sign in: add debug account

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,10 @@ BankAccount.prototype.withdrawal = function(amount){
 
 $(function(){
   var newBankAccount;
+  var debugAccount = new BankAccount("David Quiz", "dave@dave.com", "dave");
+  var tempAccount;
   var loginTry = 0;
+  var accounts = [];
 
   $("form#signup-form").submit(function(event){
     var fullName = $("#full-name").val();
@@ -27,6 +30,10 @@ $(function(){
     var password = $("#signup-password").val();
 
     newBankAccount = new BankAccount(fullName, email, password);
+    accounts.push(newBankAccount);
+
+    bankAccount1 = new BankAccount("David Quiz", "dave@dave.com", "dave");
+    accounts.push(bankAccount1);
 
     $(".show-balance").text("$" + newBankAccount.balance.toFixed(2));
     $(".person-name").text(newBankAccount.fullName);
@@ -48,12 +55,23 @@ $(function(){
     if(loginTry >= 3){
       alert("You entered wrong password too many times!")
       $("#sign-in").hide();
-    }else if(signinEmail === newBankAccount.email && signinPassword === newBankAccount.password){
+    }else if(signinEmail === debugAccount.email && signinPassword === debugAccount.password){
+      tempAccount = debugAccount;
+      $(".show-balance").text("$" + tempAccount.balance.toFixed(2));
+      $(".person-name").text(tempAccount.fullName);
+
       setTimeout(function(){
         $("#sign-in").hide();
         $("#account").show();
       }, 1000);
-
+    } else if(signinEmail === newBankAccount.email && signinPassword === newBankAccount.password){
+      tempAccount = newBankAccount;
+      $(".show-balance").text("$" + tempAccount.balance.toFixed(2));
+      $(".person-name").text(tempAccount.fullName);
+      setTimeout(function(){
+        $("#sign-in").hide();
+        $("#account").show();
+      }, 1000);
     } else {
       $("#signin-form").append("<h4>Your username and password do not match</h4>");
       loginTry++;
@@ -67,17 +85,17 @@ $(function(){
     var choice = $("#action-type").val();
     var amount = parseFloat($("#amount").val());
     if (choice === "Deposit"){
-      newBankAccount.deposit(amount);
+      tempAccount.deposit(amount);
       $("#transactions").append("<li>Deposited: $" + amount.toFixed(2) + "</li>");
-    } else if (choice === "Withdrawal" && newBankAccount.balance >= amount) {
-      newBankAccount.withdrawal(amount);
+    } else if (choice === "Withdrawal" && tempAccount.balance >= amount) {
+      tempAccount.withdrawal(amount);
       $("#transactions").append("<li>Withdrew: $" + amount.toFixed(2) + "</li>");
     } else {
       alert("You cannot withdraw more money than you have!!!!");
       $("#transactions").append("<li>Penalty: -$5.00 for wasting our time.</li>");
-      newBankAccount.withdrawal(5);
+      tempAccount.withdrawal(5);
     }
-    $(".show-balance").text("$" + newBankAccount.balance.toLocaleString(undefined, {minimumFractionDigits: 2}));
+    $(".show-balance").text("$" + tempAccount.balance.toLocaleString(undefined, {minimumFractionDigits: 2}));
     event.preventDefault();
 
   });
